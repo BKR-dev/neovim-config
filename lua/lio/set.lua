90599f16117bcff0b8867b82f5e7aa5fa7917ce1
BKR-dev <business@bkr.codes>
2023-10-18 14:37:52 +0200

    finally fixed tab with to 4 instead of 8

--- lua/lio/set.lua
+++ lua/lio/set.lua
@@ -2,8 +2,9 @@ vim.opt.termguicolors = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
-
-
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 8
 vim.opt.incsearch = true
 vim.opt.wrap = false
