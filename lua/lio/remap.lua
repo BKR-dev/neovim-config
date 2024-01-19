d82f7916ab38fe62acfc219d1af04879a989c03e
BKR-dev <business@bkr.codes>
2024-01-19 10:57:55 +0100

    improvements

--- lua/lio/remap.lua
+++ lua/lio/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ft", vim.cmd.Ex)
 vim.keymap.set("v", "<Up>", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", "<Down>", ":m '>+1<CR>gv=gv")
+vim.api.nvim_set_keymap('i','<leader><leader>w','<C-o>:w<CR>',{ noremap = true, silent = true })
