a1fd42ac8bed3ddc7dafc09fbfe13c44f2c5d4b3
BKR-dev <business@bkr.codes>
2024-02-10 14:38:46 +0100

    moving templ integration to master

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,35 +8,42 @@ lsp.nvim_workspace()
 
 -- make sure these LS are installed
 lsp.ensure_installed({
-	'gopls',
-	'eslint',
-	'lua_ls',
+    'gopls',
+    'eslint',
+    'lua_ls',
 })
 
 -- always use active LS to fromat on save
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
-	lsp.buffer_autoformat()
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
 end)
 
 require('lspconfig').gopls.setup({
-	usePlaceholders = true,
+    usePlaceholders = true,
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<cr>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<cr>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+    mapping = cmp_mappings
+})
+
+-- add templ files to lsp
+vim.filetype.add({
+    extension = {
+        templ = "templ"
+    },
 })
 
 -- sets up configuration
