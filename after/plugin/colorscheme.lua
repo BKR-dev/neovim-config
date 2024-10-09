2a7a21b48a4af46a3b3cbd25e8238b223e9bbb1c
BKR-dev <business@bkr.codes>
2024-10-09 12:52:23 +0200

    improved mappings and neckpain config, added some comments and colorscheme toggle

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,8 +1,17 @@
 function SetTheme(theme)
-	theme = theme or "palenightfall"
-	vim.cmd.colorscheme(theme)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    theme = theme or current_theme
+    vim.cmd.colorscheme(theme)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+function ToggleTheme()
+    if current_theme == "palenightfall" then
+        current_theme = "catppuccin"
+    else
+        current_theme = "palenightfall"
+    end
+    SetTheme(current_theme)
 end
 
 SetTheme()
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -85,6 +85,11 @@ _G.packer_plugins = {
     path = "/Users/lteixeir/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  catppuccin = {
+    loaded = true,
+    path = "/Users/lteixeir/.local/share/nvim/site/pack/packer/start/catppuccin",
+    url = "https://github.com/catppuccin/nvim"
+  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/Users/lteixeir/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
@@ -185,6 +190,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-surround
+time([[Config for nvim-surround]], true)
+try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
+time([[Config for nvim-surround]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
@@ -193,10 +202,6 @@ time([[Config for Comment.nvim]], false)
 time([[Config for lsp_lines.nvim]], true)
 try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14lsp_lines\frequire\0", "config", "lsp_lines.nvim")
 time([[Config for lsp_lines.nvim]], false)
--- Config for: nvim-surround
-time([[Config for nvim-surround]], true)
-try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
-time([[Config for nvim-surround]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
