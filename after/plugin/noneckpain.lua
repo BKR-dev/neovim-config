d752d7878e1500b29bc5e46b4fdb283caf67ad33
Lionel <business@bkr.codes>
2024-10-09 12:13:48 +0200

    neat NNP config

--- after/plugin/noneckpain.lua
+++ after/plugin/noneckpain.lua
@@ -1,6 +1,29 @@
 require("no-neck-pain").setup({
+    width = 100,
+    minSideBufferWidth = 10,
+    autocmds = {
+        enableOnVimEnter = true,
+    },
+    mappings = {
+        enabled = true,
+    },
     buffers = {
-        blend = -0.5,
+        -- not working for some reason!?
+        scratchPad = {
+            enabled = true,
+            location = "~/Git/ScratchPadFiles/"
+        },
+        bo = {
+            filetype = "md",
+        },
+        colors = {
+            right = {
+                backgroundColor = "tokyonight-moon",
+            },
+            left = {
+                backgroundColor = "tokyonight-moon",
+            },
+        },
         wo = {
             fillchars = "eob: ",
         }
