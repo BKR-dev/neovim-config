f8a44bfadd5d8b4ba235ec5bc16f5cde090014ec
BKR-dev <business@bkr.codes>
2024-10-10 22:13:43 +0200

    this is looking fine

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+
+vim.api.nvim_create_user_command("FindFilesInDir", function()
+    local dir = vim.env.NVIM_CWD
+    require 'telescope'.find_files({ cwd = dir })
+end, {})
