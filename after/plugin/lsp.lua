53b7eb04258b95ee79593bfaf79194debdb63b54
BKR-dev <business@bkr.codes>
2025-04-18 19:26:13 +0200

    fixing stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -53,29 +53,42 @@ require('lspconfig').yamlls.setup {
 --
 ----------- JAVA Setuo ---------------
 
+
+-- fix the tab issue 
+--
+
+
 local cmp = require('cmp')
 
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<Tab>'] = cmp.mapping(function(fallback)
-        if require("copilot.suggestion").is_visible() then
-            require("copilot.suggestion").accept()
-        elseif cmp.visible() then
-            cmp.select_next_item()
-        else
-            fallback()
-        end
-    end, { "i", "s" }),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<cr>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
+local copilot = require("copilot.suggestion") -- Ensure Copilot is properly set up
 
+cmp.setup({
+    mapping = {
+        ["<Tab>"] = function(fallback)
+            if copilot.is_visible() then
+                copilot.accept()
+            elseif cmp.visible() then
+                cmp.select_next_item()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end,
+        ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end,
+    },
+})
 
 ------------ gopls setup -----------------------
 
