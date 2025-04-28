6284d813d3ca4d35c0486998dcdd2b055eb98f53
BKR-dev <business@bkr.codes>
2025-04-28 16:50:58 +0200

    being careful about plugins state

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-# Create this file at ~/.config/nvim/setup.sh
 #!/bin/bash
+# filepath: /home/bkr/.config/nvim/setup.sh
 
 echo "Setting up Neovim configuration..."
 
@@ -13,9 +13,34 @@ if [ ! -d ~/.config/nvim/pack/packer/start/packer.nvim ]; then
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.config/nvim/pack/packer/start/packer.nvim
 fi
 
-# Run initial installation
-echo "Installing plugins - this may take a while..."
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+# Create temporary init file for installation only
+cat > ~/.config/nvim/init_setup.lua << 'EOL'
+-- Minimal init for plugin installation only
+vim.opt.packpath:prepend(vim.fn.stdpath('config'))
+vim.cmd('packadd packer.nvim')
+
+-- Basic packer setup
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'JoosepAlviste/palenightfall.nvim'
+  use 'numToStr/Comment.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'mfussenegger/nvim-dap'
+  use 'theprimeagen/harpoon'
+  -- Add other essential plugins here
+end)
+EOL
+
+# Install plugins with minimal configuration
+echo "Installing essential plugins..."
+nvim --headless -u ~/.config/nvim/init_setup.lua -c 'PackerSync' -c 'sleep 2000m' -c 'q'
+
+# Clean up temporary setup file
+rm ~/.config/nvim/init_setup.lua
+
+# Now install all plugins with regular config
+echo "Installing all plugins..."
+nvim --headless -c 'PackerSync' -c 'sleep 3000m' -c 'q'
 
 # Compile plugins
 echo "Compiling plugins..."
