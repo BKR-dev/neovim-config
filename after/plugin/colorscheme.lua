9c847f6b95b397227050b9cadf9d08738f738e78
Lionel <lionelluis.teixeirapedro@bonprix.net>
2025-05-27 17:05:33 +0200

    smaller working config

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,8 +1,14 @@
+-- vim.opt.termguicolors = true
 function SetTheme(theme)
     theme = "palenightfall"
     vim.cmd.colorscheme(theme)
+    -- keep verything nice and transparent
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- make braces. parenthesis and brackets better visible which doesnt work sadly
+    vim.api.nvim_set_hl(0, "TSBraces", { fg = "#FF00FF", bg = "#FFFFFF", bold = true })
+    vim.api.nvim_set_hl(0, "TSParenthesis", { fg = "#00FFFF", bg = "#FFFFFF", bold = true })
+    vim.api.nvim_set_hl(0, "TSBracket", { fg = "#FFFF00", bg = "#FFFFFF", bold = true })
 end
 
 SetTheme()
