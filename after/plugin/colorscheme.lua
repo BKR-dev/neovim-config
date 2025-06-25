270d7a5ade66f9c39bd04acbaa83b4076d4a20ab
Lionel <lionelluis.teixeirapedro@bonprix.net>
2025-06-25 13:07:37 +0200

    save point

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -6,9 +6,15 @@ function SetTheme(theme)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     -- make braces. parenthesis and brackets better visible which doesnt work sadly
-    vim.api.nvim_set_hl(0, "TSBraces", { fg = "#FF00FF", bg = "#FFFFFF", bold = true })
-    vim.api.nvim_set_hl(0, "TSParenthesis", { fg = "#00FFFF", bg = "#FFFFFF", bold = true })
-    vim.api.nvim_set_hl(0, "TSBracket", { fg = "#FFFF00", bg = "#FFFFFF", bold = true })
+    -- vim.api.nvim_set_hl(0, "TSBraces", { fg = "#FF00FF", bg = "#FFFFFF", bold = true })
+    -- vim.api.nvim_set_hl(0, "TSParenthesis", { fg = "#00FFFF", bg = "#FFFFFF", bold = true })
+    -- vim.api.nvim_set_hl(0, "TSBracket", { fg = "#FFFF00", bg = "#FFFFFF", bold = true })
+    vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
 end
 
 SetTheme()
