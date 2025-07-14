7b717949a190e23df2ba5ee4e9def6591d8248d1
Lionel Luis Teixeira Pedro <lionelluis.teixeirapedro@bonprix.net>
2025-07-14 15:12:37 +0200

    adding performance boost

--- lua/lio/packer.lua
+++ lua/lio/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
         -- or , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
     -- sintalling dap for debugging
     use('mfussenegger/nvim-dap')
 
