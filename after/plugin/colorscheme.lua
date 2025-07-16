d15b9a2180eafa97911f7faf0945bad25e294ede
Lionel <lionelluis.teixeirapedro@bonprix.net>
2025-07-16 10:02:53 +0200

    removed old legacy stuff

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -8,13 +8,6 @@ function SetTheme(theme)
     vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e4dbc2", bg = "#44475a", bold = true }) -- Highlight current line number
     vim.api.nvim_set_hl(0, "Comment", { fg = "#789cac", bg = "#2e3440", italic = true })    -- give comments the same look as linenumbers
     vim.api.nvim_set_hl(0, "MatchParen", { fg = "#ffffff", bg = "#ff79c6", bold = true })   -- make bracket pairs easier to see
-
-    vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
 end
 
 SetTheme()
