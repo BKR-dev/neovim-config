e8660651c48c97d84c9e24060142d4920ebca8ef
BKR-dev <business@bkr.codes>
2025-07-16 09:47:12 +0200

    this is very nice

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,9 +1,8 @@
--- vim.opt.termguicolors = true
 function SetTheme(theme)
     theme = "palenightfall"
     vim.cmd.colorscheme(theme)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })                                       -- make stuff transparent because thats nice
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })                                  -- make stuff transparent because thats nice
     vim.api.nvim_set_hl(0, "LineNr", { fg = "#789cac", bg = "#44475a" })                    -- blueish-grey foreground, dark gray background
     vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ff79c6", bg = "#44475a", bold = true }) -- Highlight current line number
     vim.api.nvim_set_hl(0, "Comment", { fg = "#789cac", bg = "#2e3440", italic = true })    -- give comments the same look as linenumbers
