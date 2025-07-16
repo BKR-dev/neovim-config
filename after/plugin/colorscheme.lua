bd361af5418c7e88c3b124466eea0145e8979687
BKR-dev <business@bkr.codes>
2025-07-16 09:44:59 +0200

    improvements to visibility

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -4,6 +4,10 @@ function SetTheme(theme)
     vim.cmd.colorscheme(theme)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#789cac", bg = "#44475a" })                    -- blueish-grey foreground, dark gray background
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ff79c6", bg = "#44475a", bold = true }) -- Highlight current line number
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#789cac", bg = "#2e3440", italic = true })    -- give comments the same look as linenumbers
+    vim.api.nvim_set_hl(0, "MatchParen", { fg = "#ffffff", bg = "#ff79c6", bold = true })   -- make bracket pairs easier to see
     vim.cmd [[
   highlight Normal guibg=none
   highlight NonText guibg=none
