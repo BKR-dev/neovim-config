2b49681fc2e7bce4e9c8cbb143c1f5c9735b31e6
BKR-dev <business@bkr.codes>
2025-07-16 09:59:35 +0200

    added some options and make things nice to look at

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,12 +1,14 @@
 function SetTheme(theme)
-    theme = "palenightfall"
+    theme = theme or "palenightfall"
     vim.cmd.colorscheme(theme)
+    -- Custom Highlihgts
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })                                       -- make stuff transparent because thats nice
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })                                  -- make stuff transparent because thats nice
     vim.api.nvim_set_hl(0, "LineNr", { fg = "#789cac", bg = "#44475a" })                    -- blueish-grey foreground, dark gray background
-    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ff79c6", bg = "#44475a", bold = true }) -- Highlight current line number
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e4dbc2", bg = "#44475a", bold = true }) -- Highlight current line number
     vim.api.nvim_set_hl(0, "Comment", { fg = "#789cac", bg = "#2e3440", italic = true })    -- give comments the same look as linenumbers
     vim.api.nvim_set_hl(0, "MatchParen", { fg = "#ffffff", bg = "#ff79c6", bold = true })   -- make bracket pairs easier to see
+
     vim.cmd [[
   highlight Normal guibg=none
   highlight NonText guibg=none
