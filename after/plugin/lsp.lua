c49e68315bbd00abac3e1ebd06f45975804b595f
BKR-dev <business@bkr.codes>
2025-08-05 20:22:34 +0200

    adding snippets dir and go snippet

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -71,7 +71,7 @@ local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
 cmp.setup({
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },                      -- LSP
