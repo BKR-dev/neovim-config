18b1a3a488b146c3be2c1876a8e7de2cea7806f7
BKR-dev <business@bkr.codes>
2025-08-15 16:58:36 +0200

    added tmux settings and removed unused deepseek config

--- lua/lio/remap.lua
+++ lua/lio/remap.lua
@@ -1,47 +1,3 @@
--- Define a Lua function to run goimports and preserve cursor position
--- local function goimports()
---     -- check if buffer is a go file
---     if vim.bo.filetype == 'go' then
---         vim.api.nvim_command('w')
---         -- Save the current cursor position
---         local cursor_pos = vim.api.nvim_win_get_cursor(0)
---         -- Run goimports on the entire buffer
---         vim.api.nvim_command('%!goimports')
---         local exit_code = vim.api.nvim_get_vvar('shell_error')
---         -- If Exit code is NOT Zero do:
---         if exit_code ~= 0 then
---             print('Undoing due to gofmt error\n')
---             -- Undo Changes
---             -- vim.api.nvim_command('undo')
---             vim.api.nvim_command('undo')
---             vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', true)
---             return
---         end
---         -- Save the File
---         vim.api.nvim_command('w')
---         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', true)
---         -- Restore the cursor position
---         vim.api.nvim_win_set_cursor(0, cursor_pos)
---     end
---     -- Save the File
---     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', true)
---     vim.api.nvim_command('w')
--- end
-
--- this isnt really working but i thought hey, why not? and the answer is : pluginslocal
--- local function addSingleComment()
---     if vim.bo.filetype == 'go' then
---         local cursor_pos = vim.api.nvim_win_get_cursor(0)
---         vim.api.nvim_command('I//<Esc>')
---         vim.api.nvim_win_set_cursor(0, cursor_pos)
---     end
---     if vim.bo.filetype == 'lua' then
---         local cursor_pos = vim.api.nvim_win_get_cursor(0)
---         vim.api.nvim_command('I--<Esc>')
---         vim.api.nvim_win_set_cursor(0, cursor_pos)
---     end
--- end
-
 vim.g.copilot_enabled = false
 function ToggleCopilot()
     if vim.g.copilot_enabled then
@@ -65,9 +21,36 @@ vim.api.nvim_set_keymap('n', '<leader>cc', ':lua ToggleCopilot()<CR>', { noremap
 vim.api.nvim_set_keymap('i', '<F20>', '<Esc>:%!goimports<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F20>', '<Esc>:%!goimports<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<F20>', '<Esc>:%!goimports<CR>', { noremap = true, silent = true })
+-- Capslock aka F13 for when inside of tmux
+vim.api.nvim_set_keymap('i', '<S-F1>', '<Esc>:w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<S-F1>', '<Esc>:w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-F1>', '<Esc>:w<CR>', { noremap = true, silent = true })
+-- Capslock aka F13 for when not inside of tmux
 vim.api.nvim_set_keymap('i', '<F13>', '<Esc>:w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<F13>', '<Esc>:w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F13>', '<Esc>:w<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<F13>', ':GoImports<CR>', { noremap = true, silent = true })
 -- toggle undotree on leader-u
 vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR><C-w>h', { noremap = true, silent = true })
+-- move between tmux windows
+-- Function to send a tmux command
+local function tmux_command(cmd)
+    vim.fn.system("tmux " .. cmd)
+end
+
+-- Map <Leader>t<number> to switch to a specific tmux window
+for i = 1, 9 do
+    vim.keymap.set("n", "<Leader>t" .. i, function()
+        tmux_command("select-window -t " .. i)
+    end, { desc = "Switch to tmux window " .. i })
+end
+
+-- Map <Leader>n to switch to the next tmux window
+vim.keymap.set("n", "<Leader>n", function()
+    tmux_command("next-window")
+end, { desc = "Switch to the next tmux window" })
+
+-- Map <Leader>p to switch to the previous tmux window
+vim.keymap.set("n", "<Leader>p", function()
+    tmux_command("previous-window")
+end, { desc = "Switch to the previous tmux window" })
