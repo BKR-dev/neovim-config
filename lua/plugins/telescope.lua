2869c9c672d2a5e5e93ed4d62214d9de914a823a
BKR-dev <business@bkr.codes>
2025-12-17 10:43:52 +0100

    updated TS version for fixes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
+        branch = 'master',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
