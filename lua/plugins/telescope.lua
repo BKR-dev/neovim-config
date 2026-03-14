c98a699719c1436e01afb878cba5f1edb1a0816b
BKR-dev <business@bkr.codes>
2026-03-14 23:05:17 +0100

    fix: pin telescope to 0.1.8 tag to resolve flatten nil error on nvim 0.10+

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        branch = 'master',
+        tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
